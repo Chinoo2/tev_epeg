b4da782c822ccaecbd40806d58819ce71b616e8e
Chinoo2 <trabajocompu29@gmail.com>
2026-08-20 22:14:27 -0300

    Update generar_tev_epg.py

--- generar_tev_epg.py
+++ generar_tev_epg.py
@@ -1,4 +1,4 @@
-```python
+
 import requests
 import xml.etree.ElementTree as ET
 from datetime import datetime, timezone
@@ -576,4 +576,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-```
+
